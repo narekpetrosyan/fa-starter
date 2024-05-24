3ed3988a9be6f65c477fb187c8239beaa3d9c7c1
narekpetrosyan <narek.petrosyan988@gmail.com>
2024-05-24 14:35:29 +0400

    add database helper

--- fastapi-application/core/config.py
+++ fastapi-application/core/config.py
@@ -1,9 +1,16 @@
 from pydantic_settings import BaseSettings
-from pydantic import BaseModel
+from pydantic import BaseModel, PostgresDsn
 
 class RunConfig(BaseModel):
     host: str = "0.0.0.0"
     port: int = 8000
+
+class DatabaseConfig(BaseModel):
+    url:PostgresDsn
+    echo: bool = False
+    echo_pool: bool = False
+    pool_size:int = 5
+    max_overflow:int = 10
 
 class ApiPrefix(BaseModel):
     prefix: str = "/api"
@@ -11,5 +18,6 @@ class ApiPrefix(BaseModel):
 class Settings(BaseSettings):
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
+    db: DatabaseConfig
 
 settings = Settings()
