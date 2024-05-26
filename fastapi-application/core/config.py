c5bf7598f7ddf8365bfab7400863163221f379ff
narekpetrosyan <narekpetrosyan199823@gmail.com>
2024-05-26 17:31:39 +0400

    add naming conventions

--- fastapi-application/core/config.py
+++ fastapi-application/core/config.py
@@ -14,6 +14,14 @@ class DatabaseConfig(BaseModel):
     pool_size: int = 5
     max_overflow: int = 10
 
+    naming_conventions: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s"
+    }
+
 
 class ApiPrefix(BaseModel):
     prefix: str = "/api"
