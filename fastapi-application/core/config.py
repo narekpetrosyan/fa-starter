5b54a358007d7345a87eb81f924a0f67c4e4dc71
narekpetrosyan <narekpetrosyan199823@gmail.com>
2024-05-26 17:14:46 +0400

    install alembic + black

--- fastapi-application/core/config.py
+++ fastapi-application/core/config.py
@@ -1,19 +1,23 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import BaseModel, PostgresDsn
 
+
 class RunConfig(BaseModel):
     host: str = "0.0.0.0"
     port: int = 8000
 
+
 class DatabaseConfig(BaseModel):
-    url:PostgresDsn
+    url: PostgresDsn
     echo: bool = False
     echo_pool: bool = False
-    pool_size:int = 5
-    max_overflow:int = 10
+    pool_size: int = 5
+    max_overflow: int = 10
+
 
 class ApiPrefix(BaseModel):
     prefix: str = "/api"
+
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
@@ -25,5 +29,6 @@ class Settings(BaseSettings):
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
     db: DatabaseConfig
+
 
 settings = Settings()
